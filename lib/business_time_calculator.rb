7c0e5f87b855300f4f16b79fb6efa0c50eb43f16
Jordi Noguera <jordinoguera83@gmail.com>
2016-11-01 22:24:23 +0100

    Rename project files to business time calculator

--- lib/business_time_calculator.rb
+++ lib/business_time_calculator.rb
@@ -2,5 +2,5 @@ require 'better_business_time/weekday_holidays'
 require 'better_business_time/time_decorator'
 require 'better_business_time/time'
 
-module BetterBusinessTime
+module BusinessTimeCalculator
 end
