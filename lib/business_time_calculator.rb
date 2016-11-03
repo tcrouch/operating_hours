f35ac472b04074448f9545c669d53638e53f9219
Jordi Noguera <jordinoguera83@gmail.com>
2016-11-03 19:34:31 +0100

    Rename business time calculator file to calculator

--- lib/business_time_calculator.rb
+++ lib/business_time_calculator.rb
@@ -1,5 +1,5 @@
 require 'business_time_calculator/version'
-require 'business_time_calculator/business_time_calculator'
+require 'business_time_calculator/calculator'
 require 'business_time_calculator/work_day'
 require 'business_time_calculator/schedule'
 require 'business_time_calculator/holiday_collection'
