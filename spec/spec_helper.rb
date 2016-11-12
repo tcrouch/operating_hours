e9e71ca2f6db179d7ad9e86ca57fc1593d0451c2
Jordi Noguera <jordinoguera83@gmail.com>
2016-11-12 18:50:28 +0100

    check weekday if passed in date

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,23 @@ require 'pry'
 require_relative '../lib/business_time_calculator'
 
 def d(string)
-  Date.parse("#{string} 2016")
+  check_weekday(string) do
+    Date.parse("#{string} 2016")
+  end
 end
 
 def t(date_string, time_string)
-  Time.parse("#{date_string} 2016 #{time_string}")
+  check_weekday(date_string) do
+    Time.parse("#{date_string} 2016 #{time_string}")
+  end
+end
+
+def check_weekday(string)
+  wday_word = string.split(' ')[0].to_s.downcase
+  wday = BusinessTimeCalculator::Schedule::WDAYS[wday_word]
+  date_or_time = yield
+  if wday && date_or_time.wday != wday
+    raise 'Wrong weekday entered'
+  end
+  date_or_time
 end
